d651f583fb8e90ba24c5f39d1473671c0a3b84af
AriesButTheUsernameWasAlreadyTaken <ariesgeorge2001@gmail.com>
2024-12-09 19:05:55 +0100

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//#include <iostream> (cpp syntax)
+//#include <string> (cpp syntax)
+//using namespace std; (cpp syntax)
+
+int main ()
+{
+    printf("\nHello Wordl\n");
+    printf("i'm running out of patience\n");
+
+return 0;
+}
+//g++ -o main.exe main.cpp (cpp syntax) 
+//string msg = "Hello World" (cpp syntax)
+//cout << msg << endl; (cpp syntax) 
