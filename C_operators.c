a99044413a2689d89458ba2e078f35591ff02fda
Ariès <ariesgeorge2001@gmail.com>
2025-01-19 19:50:20 +0100

    Add files via upload

---
+++ C_operators.c
@@ -0,0 +1,30 @@
+/*
+In this file, I'll see operators, and how the differents variables types reacts to them, and to operations. 
+This will also lead to maybe the creation of another file in which I'll dig variables conversions.
+Or i'll dig it on this file, we'll see. But it will be learnt. 
+Anyway. here's the base tutorial followed here : 
+https://www.youtube.com/watch?v=9E3I1URu6fc&list=PLZPZq0r_RZOOzY_vR4zJM32SqsSInGMwe&index=9
+And this is the wikipedia article that we will also follow and dig : 
+https://en.wikipedia.org/wiki/Operators_in_C_and_C%2B%2B
+And yes, i'll like to see every one of them, try to break them, but mostly understand how they interact. 
+With erery type of variable. we're in for a long ride. 
+We may learn quite a few things within it. Will it be fun ? Well, for me, surely. 
+*/
+
+
+#include <stdio.h>
+int main(){
+printf("======================Arithmetic operators zone======================");
+/*
+let's first see additions. With ints. 
+*/
+
+int a = 10;
+int b = 12;
+int c = 7;
+int d = 800;
+
+
+
+return 0; 
+}
